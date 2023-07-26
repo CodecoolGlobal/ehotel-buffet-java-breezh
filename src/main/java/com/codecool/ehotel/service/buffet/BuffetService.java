6e3686f15fcbaa1219bb71359b50379085997551
breezh <benii1030@gmail.com>
2023-07-26 13:43:06 +0200

    add collectWaste method to BuffetServiceImpl class

--- src/main/java/com/codecool/ehotel/service/buffet/BuffetService.java
+++ src/main/java/com/codecool/ehotel/service/buffet/BuffetService.java
@@ -4,11 +4,14 @@ import com.codecool.ehotel.model.Buffet;
 import com.codecool.ehotel.model.Meal;
 import com.codecool.ehotel.model.MealType;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface BuffetService {
     public void refill(Meal meal, int amount, Buffet buffet);
 
     public boolean consumeFreshest(Buffet buffet, MealType mealType);
+
+    public int collectWaste(Buffet buffet, LocalDate date);
 
 }
--- src/main/java/com/codecool/ehotel/service/buffet/BuffetServiceImpl.java
+++ src/main/java/com/codecool/ehotel/service/buffet/BuffetServiceImpl.java
@@ -2,11 +2,13 @@ package com.codecool.ehotel.service.buffet;
 
 import com.codecool.ehotel.model.Buffet;
 import com.codecool.ehotel.model.Meal;
+import com.codecool.ehotel.model.MealDurability;
 import com.codecool.ehotel.model.MealType;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public class BuffetServiceImpl implements BuffetService{
+public class BuffetServiceImpl implements BuffetService {
     Buffet buffet;
 
     public BuffetServiceImpl(Buffet buffet) {
@@ -30,5 +32,29 @@ public class BuffetServiceImpl implements BuffetService{
             }
         }
         return false;
+    }
+
+    @Override
+    public int collectWaste(Buffet buffet, LocalDate date) {
+        int totalCost = 0;
+        for (Meal meal : buffet.meals()) {
+            if (expirationDate(meal).isAfter(date)) {
+                totalCost += meal.mealType().getCost();
+                buffet.meals().remove(meal);
+            }
+        }
+        return totalCost;
+    }
+
+    private int expirationDays(MealDurability mealDurability) {
+        return switch (mealDurability) {
+            case SHORT -> 1;
+            case MEDIUM -> 2;
+            case LONG -> 3;
+        };
+    }
+
+    private LocalDate expirationDate(Meal meal) {
+        return meal.creationTime().plusDays(expirationDays(meal.mealType().getDurability()));
     }
 }
